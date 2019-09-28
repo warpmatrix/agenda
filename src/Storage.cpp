513243799478c4ee6734efefc164cc9b2ee284a8
warpmatrix <administrator@host-172-18-229-255.local>
2019-09-28 09:54:05 -0400

    build storage2

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -1,13 +1,24 @@
 #include "Storage.hpp"
 
-// std::shared_ptr<Storage> Storage::m_instance;
+std::shared_ptr<Storage> Storage::m_instance = nullptr;
 
-// Storage::Storage() {
-// 	m_dirty = false;
-// 	readFromFile();
-// }
+Storage::Storage() {
+	m_dirty = false;
+	readFromFile();	
+}
 std::shared_ptr<Storage> Storage::getInstance(void) {
 	if (!m_instance)
 		m_instance.reset(new Storage() );
 	return m_instance;
+}
+
+bool Storage::readFromFile(void) {
+	ifstream fs;
+
+}
+bool Storage::writeToFile(void) {
+
+}
+int Storage::deleteUser(std::function<bool(const User &)> filter) {
+
 }
